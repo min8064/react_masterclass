e56738144789cea0eabfe91c67c5f7a062ee6f2d
lsm <lsm@gmail.com>
2022-05-20 15:56:30 +0900

    Variants

--- react_animations/src/App.tsx
+++ react_animations/src/App.tsx
@@ -1,4 +1,3 @@
-import { MotionConfig } from "framer-motion";
 import styled from "styled-components";
 import {motion} from "framer-motion";
 
@@ -14,15 +13,62 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+// transition type: "spring", damping : 반동력 0이면 무기한 진동, stiffness : 경직성, mass : 물체의 질량, 
+// bounce : 0~1사이 값이여야함,  0.25가 기본값, 작을수록 덜 튕김
 function App() {
   return(
     <Wrapper>
-      <Box initial={{ scale:0 }} animate={{ scale:1 }}/>
+      <Box variants={boxVariants} initial="start" animate="end" >
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
