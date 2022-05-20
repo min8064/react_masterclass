bca9f100501b84e69417608cbae273a38d716327
lsm <lsm@gmail.com>
2022-05-20 16:08:34 +0900

    whilehover, whiletap

--- react_animations/src/App.tsx
+++ react_animations/src/App.tsx
@@ -15,47 +15,12 @@ const Box = styled(motion.div)`
   height: 200px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
 };
 
 // transition type: "spring", damping : 반동력 0이면 무기한 진동, stiffness : 경직성, mass : 물체의 질량, 
@@ -63,12 +28,7 @@ const circleVariants = {
 function App() {
   return(
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end" >
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box whileHover={{ scale: 2 }} whileTap={{ borderRadius: "100px" }} />
     </Wrapper>
   );
 }
