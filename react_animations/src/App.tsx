609b3dafa2410400801d1ca072b873f8ffeeb972
min8064 <59388162+min8064@users.noreply.github.com>
2022-05-21 04:22:35 +0900

    animation code challenge

--- react_animations/src/App.tsx
+++ react_animations/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -8,25 +8,41 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  background: linear-gradient(#e66465, #9198e5);
+  flex-direction: column;
 `;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
+  grid-template-columns: repeat(2, 1fr);
+  width: 550px;
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
 `;
 
 const Box = styled(motion.div)`
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
   height: 200px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Circle = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 1);
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled(motion.button)`
+  margin-top: -220px;
+  color: rgba(230, 126, 34, 1);
+  border-radius: 5px;
+  height: 35px;
+`
 
 const Overlay = styled(motion.div)`
   width: 100%;
@@ -39,19 +55,60 @@ const Overlay = styled(motion.div)`
 
 const overlay = {
   hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
-  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
-  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    transition: { duration: 0.3 }
+  },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)", transition: { duration: 0.3 } }
+};
+
+const boxVariants = {
+  hover: (isHover:number) => ({
+    scale: 1.1,
+    x: isHover === 0 ? -15 : 15,
+    y: isHover === 0 ? -10 : 10
+  }),
+  tap: {
+    color: "rgba(41, 128, 185, 1.0)",
+    scale: 1.3,
+    transition: { duration: 0.2 }
+  }
 };
 
 function App() {
   const [id, setId] = useState<null | string>(null);
-  return(
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
+
+  return (
     <Wrapper>
-       <Grid>
-        {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
-        ))}
+      <Grid>
+        <Box
+          onClick={() => setId("1")}
+          key={"1"}
+          layoutId={"1"}
+          variants={boxVariants}
+          whileHover="hover"
+          custom={0}
+        />
+        <Box onClick={() => setId("2")} key={"2"} layoutId={"2"}>
+          {!clicked ? <Circle layoutId="circle" /> : null}
+        </Box>
+        <Box onClick={() => setId("3")} key={"3"} layoutId={"3"}>
+          {clicked ? <Circle layoutId="circle" /> : null}
+        </Box>
+        <Box
+          onClick={() => setId("4")}
+          key={"4"}
+          layoutId={"4"}
+          variants={boxVariants}
+          whileHover="hover"
+          custom={1}
+        />
       </Grid>
+      <Button onClick={toggleClicked} variants={boxVariants} whileTap="tap">
+        Switch
+      </Button>
       <AnimatePresence>
         {id ? (
           <Overlay
@@ -61,7 +118,14 @@ function App() {
             animate="visible"
             exit="exit"
           >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+            <Box
+              layoutId={id}
+              style={{
+                width: "40vw",
+                height: 200,
+                backgroundColor: "rgba(255,255,255,1)"
+              }}
+            />
           </Overlay>
         ) : null}
       </AnimatePresence>
